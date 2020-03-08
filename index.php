74f765b9bd8471306790cd897e7994bc1404a059
RachidLh <RachidLh@users.noreply.github.com>
2020-03-08 13:52:41 +0100

    ok

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/all.css">
-</head>
-<body>
-    
-<?php include "includes/menu.php" ?>
+<?php
+ include "includes/head.php";
+ include "includes/menu.php";
+?>
+
+<div class="container-fluid dashboard">
+    <div class="row">
+        <div class="col-lg-12 text-white pt-2 pb-2 header">
+            <h4>Dashboard</h4>
+        </div>
+    </div>
+    <div class="row content mt-5">
+        <div class="col-lg-12">
+            <div class="alert alert-success" role="alert">
+                <h4 class="alert-heading">Well done!</h4>
+                <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+                <hr>
+                <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+            </div>
+        </div>
+    </div>
+</div>
 
 
-
-
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/bootstrap.bundle.js"></script>
-</body>
-</html>
+<?php include "includes/footer.php"; ?>
